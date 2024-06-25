54e661f136fa82b7fea858b5d38147b5b14ea406
gilbmporto <gilbmp@gmail.com>
2024-06-25 19:39:21 -0300

    Finished Dropdown menu component

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
 	DropdownMenu,
 	DropdownMenuContent,
@@ -5,32 +7,68 @@ import {
 	DropdownMenuTrigger,
 	DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu"
+import {
+	SignedIn,
+	SignedOut,
+	SignInButton,
+	SignUpButton,
+	useUser,
+} from "@clerk/nextjs"
 import { LuAlignLeft } from "react-icons/lu"
 import { Button } from "../ui/button"
 import { links } from "@/utils/links"
 import Link from "next/link"
 import UserIcon from "./UserIcon"
 import SignOutLink from "./SignOutLink"
+import Image from "next/image"
 
 function LinksDropdown() {
+	const user = useUser()
+
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
 				<Button variant="outline" className="flex gap-4 max-w-[100px]">
 					<LuAlignLeft className="w-6 h-6" />
-					<UserIcon />
+					{user.isLoaded && user.isSignedIn && user.user.hasImage ? (
+						<Image
+							src={user.user.imageUrl}
+							alt={user.user.firstName!}
+							width={24}
+							height={24}
+							className="object-cover rounded-full"
+						/>
+					) : (
+						<UserIcon />
+					)}
 				</Button>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent className="w-52" align="start" sideOffset={10}>
-				{links.map((link) => (
-					<DropdownMenuItem key={link.href}>
-						<Link href={link.href} className="capitalize w-full">
-							{link.label}
-						</Link>
+				<SignedOut>
+					<DropdownMenuItem>
+						<SignInButton mode="modal">
+							<button className="w-full text-left ">Login</button>
+						</SignInButton>
 					</DropdownMenuItem>
-				))}
-				<DropdownMenuSeparator />
-				<SignOutLink />
+					<DropdownMenuItem>
+						<SignUpButton mode="modal">
+							<button className="w-full text-left ">Register</button>
+						</SignUpButton>
+					</DropdownMenuItem>
+				</SignedOut>
+				<SignedIn>
+					{links.map((link) => (
+						<DropdownMenuItem key={link.href}>
+							<Link href={link.href} className="capitalize w-full">
+								{link.label}
+							</Link>
+						</DropdownMenuItem>
+					))}
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>
+						<SignOutLink />
+					</DropdownMenuItem>
+				</SignedIn>
 			</DropdownMenuContent>
 		</DropdownMenu>
 	)
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "img.clerk.com",
+			},
+		],
+	},
 }
 
 export default nextConfig
