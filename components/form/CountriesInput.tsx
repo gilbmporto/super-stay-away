9b26ec90a651b8db8a754c7322735ba59d5a579c
gilbmporto <gilbmp@gmail.com>
2024-07-02 15:56:52 -0300

    Add countries package | Developed countries.ts helper script | Developed CountriesInput.tsx component | Implemented Country and Image Inputs inside rentals/create page

---
+++ components/form/CountriesInput.tsx
@@ -0,0 +1,44 @@
+// ui components
+import { Label } from "../ui/label"
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "../ui/select"
+
+// countries list
+import { formattedCountries } from "@/utils/countries"
+
+function CountriesInput({ defaultValue }: { defaultValue?: string }) {
+	const name = "country"
+
+	return (
+		<div className="mb-2">
+			<Label htmlFor={name} className="capitalize">
+				Country
+			</Label>
+			<Select
+				defaultValue={defaultValue ?? formattedCountries[0].code}
+				name={name}
+				required
+			>
+				<SelectTrigger id={name}>
+					<SelectValue />
+				</SelectTrigger>
+				<SelectContent>
+					{formattedCountries.map((country) => (
+						<SelectItem key={country.code} value={country.code}>
+							<span className="flex items-center gap-2">
+								{country.flag} {country.name}
+							</span>
+						</SelectItem>
+					))}
+				</SelectContent>
+			</Select>
+		</div>
+	)
+}
+
+export default CountriesInput
