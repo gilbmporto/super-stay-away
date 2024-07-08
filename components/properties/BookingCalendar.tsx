4787f11328fa8f17f3d7c770e038bcc7cc219734
gilbmporto <gilbmp@gmail.com>
2024-07-08 14:06:51 -0300

    Developed 'BookingCalendar' component | Included it in 'properties/[id]' page

---
+++ components/properties/BookingCalendar.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+// hooks
+import { useState } from "react"
+
+// components
+import { Calendar } from "../ui/calendar"
+import { DateRange } from "react-day-picker"
+
+function BookingCalendar() {
+	const currentDate = new Date()
+	const defaultSelected: DateRange = {
+		from: undefined,
+		to: undefined,
+	}
+
+	const [range, setRange] = useState<DateRange | undefined>(defaultSelected)
+
+	return (
+		<Calendar
+			mode="range"
+			defaultMonth={currentDate}
+			selected={range}
+			onSelect={setRange}
+			className="-mt-2"
+		/>
+	)
+}
+
+export default BookingCalendar
