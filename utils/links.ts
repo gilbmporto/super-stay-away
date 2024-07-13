d8d2abf035a9e2af1780436804bdfd0441005c03
gilbmporto <gilbmp@gmail.com>
2024-07-13 13:22:17 -0300

    Fixed 'loading' page of '/reservations' | Add '/admin' page

--- utils/links.ts
+++ utils/links.ts
@@ -11,5 +11,6 @@ export const links: NavLink[] = [
 	{ href: "/reservations", label: "Reservations" },
 	{ href: "/rentals/create", label: "Create Rental" },
 	{ href: "/rentals", label: "Rentals" },
+	{ href: "/admin", label: "Admin" },
 	{ href: "/profile", label: "Profile" },
 ]
