1327a1abbf1c3634db88209a62defca5cc0b0ad2
gilbmporto <gilbmp@gmail.com>
2024-07-15 16:11:26 -0300

    Developed 'checkout' page with stripe as a the payment provider

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,9 +1,43 @@
-import React from "react"
+"use client"
+
+// axios is better than fetch
+import axios from "axios"
+
+// nextjs | hooks imports
+import { useSearchParams } from "next/navigation"
+import React, { useCallback } from "react"
+
+// stripe imports
+import { loadStripe } from "@stripe/stripe-js"
+import {
+	EmbeddedCheckoutProvider,
+	EmbeddedCheckout,
+} from "@stripe/react-stripe-js"
+
+const stripePromise = loadStripe(
+	process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE as string
+)
 
 function CheckoutPage() {
+	const searchParams = useSearchParams()
+	const bookingId = searchParams.get("bookingId")
+
+	const fetchClientSecret = useCallback(async () => {
+		// Create a new Checkout Session
+		const res = await axios.post("/api/payment", {
+			bookingId,
+		})
+		return res.data.clientSecret
+	}, [])
+
+	const options = { fetchClientSecret }
+
 	return (
-		<div>
-			<h1 className="text-4xl">Checkout Page</h1>
+		<div id="checkout">
+			{/* Stripe Checkout */}
+			<EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+				<EmbeddedCheckout />
+			</EmbeddedCheckoutProvider>
 		</div>
 	)
 }
