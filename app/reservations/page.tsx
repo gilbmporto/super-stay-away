a02178f0481111adae5693b3204255927fb8b02f
gilbmporto <gilbmp@gmail.com>
2024-07-24 21:17:32 -0300

    Add 'Stats' component to '/reservations' page | Developed 'fetchReservationStats' server action to fetch data to 'Stats' component

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -5,6 +5,7 @@ import { fetchReservations } from "@/utils/actions"
 import Link from "next/link"
 import EmptyList from "@/components/home/EmptyList"
 import CountryFlagAndName from "@/components/card/CountryFlagAndName"
+import Stats from "@/components/reservations/Stats"
 
 // ui components
 import {
@@ -28,55 +29,58 @@ async function ReservationsPage() {
 	}
 
 	return (
-		<div className="mt-16">
-			<h4 className="mb-4 capitalize">
-				Total Reservations: {reservations?.length}
-			</h4>
-			<Table>
-				<TableCaption>A list of your recent reservations</TableCaption>
-				<TableHeader>
-					<TableRow>
-						<TableHead>Property Name</TableHead>
-						<TableHead>Country</TableHead>
-						<TableHead>Nights</TableHead>
-						<TableHead>Total</TableHead>
-						<TableHead>Check In</TableHead>
-						<TableHead>Check Out</TableHead>
-					</TableRow>
-				</TableHeader>
-				<TableBody>
-					{reservations &&
-						reservations.map((reservation) => {
-							const { id, orderTotal, totalNights, checkIn, checkOut } =
-								reservation
-							const { id: propertyId, name, country } = reservation.property
+		<>
+			<Stats />
+			<div className="mt-16">
+				<h4 className="mb-4 capitalize">
+					Total Reservations: {reservations?.length}
+				</h4>
+				<Table>
+					<TableCaption>A list of your recent reservations</TableCaption>
+					<TableHeader>
+						<TableRow>
+							<TableHead>Property Name</TableHead>
+							<TableHead>Country</TableHead>
+							<TableHead>Nights</TableHead>
+							<TableHead>Total</TableHead>
+							<TableHead>Check In</TableHead>
+							<TableHead>Check Out</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{reservations &&
+							reservations.map((reservation) => {
+								const { id, orderTotal, totalNights, checkIn, checkOut } =
+									reservation
+								const { id: propertyId, name, country } = reservation.property
 
-							const startDate = formatDate(checkIn)
-							const endDate = formatDate(checkOut)
+								const startDate = formatDate(checkIn)
+								const endDate = formatDate(checkOut)
 
-							return (
-								<TableRow key={id}>
-									<TableCell>
-										<Link
-											href={`/properties/${propertyId}`}
-											className="underline text-muted-foreground tracking-wide"
-										>
-											{name}
-										</Link>
-									</TableCell>
-									<TableCell>
-										<CountryFlagAndName countryCode={country} />
-									</TableCell>
-									<TableCell>{totalNights}</TableCell>
-									<TableCell>{formatCurrency(orderTotal)}</TableCell>
-									<TableCell>{startDate}</TableCell>
-									<TableCell>{endDate}</TableCell>
-								</TableRow>
-							)
-						})}
-				</TableBody>
-			</Table>
-		</div>
+								return (
+									<TableRow key={id}>
+										<TableCell>
+											<Link
+												href={`/properties/${propertyId}`}
+												className="underline text-muted-foreground tracking-wide"
+											>
+												{name}
+											</Link>
+										</TableCell>
+										<TableCell>
+											<CountryFlagAndName countryCode={country} />
+										</TableCell>
+										<TableCell>{totalNights}</TableCell>
+										<TableCell>{formatCurrency(orderTotal)}</TableCell>
+										<TableCell>{startDate}</TableCell>
+										<TableCell>{endDate}</TableCell>
+									</TableRow>
+								)
+							})}
+					</TableBody>
+				</Table>
+			</div>
+		</>
 	)
 }
 
